b3ce63d0c0a633a08d8360ea09aa466497565396
Andy-Ops254 <ghostandy254@outlook.com>
2025-06-24 14:57:24 +0300

    complete the simple liker app

--- main.js
+++ main.js
@@ -3,6 +3,47 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const eModal =document.getElementById('modal')
+eModal.classList.add('hidden') //classList.add() lets you work with css elements in javascript
+
+const likes = document.getElementsByClassName('like-glyph');
+Array.from(likes).forEach(like =>{// the method Array.from() converts a list of items to objects
+  like.addEventListener('click', handleClick)
+});
+
+function handleClick(event) {
+  const heart= event.target;
+  if(heart.classList.contains('activated-heart')) {
+    unlikeHeart(heart)
+  }
+  else{
+    likeHeart(heart)
+  }
+}
+
+function likeHeart(heart) {
+  mimicServerCall()
+  .then(()=> {
+    heart.textContent=FULL_HEART;
+    heart.classList.add('activated-heart'); //this method,adds class to the element
+  })
+  .catch (() => {
+    eModal.classList.remove('hidden'); //this method, removes class to the element
+    setTimeout(() => eModal.classList.add('hidden'),2000);
+  });
+};
+
+function unlikeHeart(heart) {
+  mimicServerCall()
+  .then(()=> {
+    heart.textContent=EMPTY_HEART;
+    heart.classList.remove('activated-heart');
+  })
+  .catch (() => {
+    eModal.classList.remove('hidden');
+    setTimeout(() => eModal.classList.add('hidden'),2000);
+  });
+} //classList.contains('class-name')checks if the element has a specific class.
 
 
 
